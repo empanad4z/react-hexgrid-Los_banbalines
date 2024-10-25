4b948ebcfb3a6f317ee37b095bda26b4620b1b57
David Parra <davidparra812@gmail.com>
2024-10-25 16:28:16 -0300

    fix error typescript

--- src/HexGrid.tsx
+++ src/HexGrid.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React from "react"
 
 type Props = {} & React.SVGProps<SVGSVGElement>
 
--- src/models/Point.ts
+++ src/models/Point.ts
@@ -1,7 +1,7 @@
 export class Point {
   public x: number
   public y: number
-  constructor(x, y) {
+  constructor(x:number, y:number) {
     this.x = x
     this.y = y
   }
